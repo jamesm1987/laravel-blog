080dee79c8bc1f29a3cc7a8460704f44d80e7e54
James McCormack <james@wirebox.co.uk>
2022-09-07 23:00:27 +0100

    create post layout

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -7,8 +7,8 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+    <div class="my-5 w-full flex flex-col space-y-4 md:flex-row md:space-y-0">
+        <div class="md:w-2/3 lg:w-3/4 px-5">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <h1>New Post</h1>
@@ -26,30 +26,31 @@
             </div>
         </div>
 
-        <div class="max-w-2xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <h2>Publish</h2>
+        <aside class="md:w-1/3 lg:w-1/4">
+            <div class="max-w-2xl mx-auto sm:px-6 lg:pl-0 lg:pr-6">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <h2>Publish</h2>
+                    </div>
                 </div>
-            </div>
-        </div>      
+            </div>      
 
-        <div class="max-w-2xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <h2>Categories</h2>
+            <div class="max-w-2xl mx-auto sm:px-6 lg:pl-0 lg:pr-6">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <h2>Categories</h2>
+                    </div>
                 </div>
-            </div>
-        </div>    
-        
-        <div class="max-w-2xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <h2>Tags</h2>
+            </div>    
+            
+            <div class="max-w-2xl mx-auto sm:px-6 lg:pl-0 lg:pr-6">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <h2>Tags</h2>
+                    </div>
                 </div>
-            </div>
-        </div>            
-        
+            </div>            
+        </aside>
     </div>
 
     <script src="https://cdn.ckeditor.com/ckeditor5/35.1.0/classic/ckeditor.js"></script>
